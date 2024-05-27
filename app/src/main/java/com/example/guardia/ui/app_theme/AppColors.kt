0bd0d7d9e2d7867b0ac7fa7d53e1214236899889
144055 <anacrispee@gmail.com>
2024-05-26 22:12:54 -0300

    Feature: updating app theme according to the Figma design

--- app/src/main/java/com/example/guardia/ui/app_theme/AppColors.kt
+++ app/src/main/java/com/example/guardia/ui/app_theme/AppColors.kt
@@ -14,17 +14,17 @@ class AppColors(
 }
 
 object Primary {
+    val darker_pink = Color(0xFFA2113D)
     val dark_pink = Color(0xFFB81D4C)
-    val light_pink = Color(0xFFD24F76)
+    val light_pink = Color(0xFFC86180)
     val lighter_pink = Color(0xFFF9E8ED)
-    val darkest_pink = Color(0xFFA2113D)
     val dark_grey = Color(0xFF5F5F5F)
     val light_grey = Color(0xFF969696)
+    val lighter_grey = Color(0xFFD9D9D9)
 }
 
 object Secondary {
     val background = Color(0xFFF5F5F5)
-    val lighter_grey = Color(0xFFD9D9D9)
     val lighter = Color(0xFFFFFFFF)
 }
 

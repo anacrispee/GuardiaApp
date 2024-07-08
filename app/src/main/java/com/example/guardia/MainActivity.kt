b3dddc0ab2ed72dcf86dadbedf28b1ca2b56e1d7
Ana Cristina <anacrispee@edu.univali.br>
2024-07-08 20:30:58 -0300

    Refactor: applying innerPadding to fit content with de bottom bar.

--- app/src/main/java/com/example/guardia/MainActivity.kt
+++ app/src/main/java/com/example/guardia/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Badge
@@ -111,10 +113,15 @@ class MainActivity : ComponentActivity() {
                             }
                         }
                     }
-                ) {
-                    NavigationGraph(
-                        navController = navController
-                    )
+                ) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                    ) {
+                        NavigationGraph(
+                            navController = navController
+                        )
+                    }
                 }
             }
         }
